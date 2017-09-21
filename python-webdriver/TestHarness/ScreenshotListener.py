bc2c978f281da13a464c884d00b478f498c7c6cc
Sachin Pathare <spathare@redhat.com>
2017-09-21 23:31:27 +1000

    Screenshot and logging fixes

--- python-webdriver/TestHarness/ScreenshotListener.py
+++ python-webdriver/TestHarness/ScreenshotListener.py
@@ -1,14 +1,18 @@
-from selenium.webdriver.support.events import AbstractEventListener
-import time
 import logging
 import os
+import time
+from PIL import Image
+from PIL import ImageDraw
+import io
+from selenium.webdriver.support.events import AbstractEventListener
+
+from TestConfig import TestRunConfig
+
 
 class ScreenshotListener(AbstractEventListener):
 
     def __init__(self):
-        self.basename = "/tmp/screenshots/"
-        if "SCREENSHOTDIR" in os.environ:
-            self.basename = os.environ.get("SCREENSHOTDIR")
+        self.basename = TestRunConfig.TestRunConfig().get("screenshot_dir")
         assert(not os.path.isfile(self.basename))
         if not str(self.basename).endswith("/"):
             self.basename += "/"
@@ -16,6 +20,7 @@ class ScreenshotListener(AbstractEventListener):
             os.makedirs(self.basename)
 
     def on_exception(self, exception, driver):
+        logging.info("Exception: " + exception.msg)
         self.take_screenshot(driver, '_exception')
 
     def before_click(self, element, driver):
@@ -34,7 +39,28 @@ class ScreenshotListener(AbstractEventListener):
         self.take_screenshot(driver, '_post-nav')
 
     def take_screenshot(self, driver, name):
-        time.sleep(1)
+        time.sleep(0.25)
         name = self.basename + str(time.time()) + name + '.png'
         driver.get_screenshot_as_file(name)
         logging.info("Screenshot saved as '%s'" % name)
+
+    @staticmethod
+    def custom_screenshot(driver, name):
+        instance = ScreenshotListener()
+        ScreenshotListener.take_screenshot(instance, driver, name)
+
+    @staticmethod
+    def annotated_screenshot(driver, details):
+        instance = ScreenshotListener()
+        data = driver.get_screenshot_as_png()
+        image = Image.open(io.BytesIO(data))
+        draw = ImageDraw.Draw(image)
+        x = details.get('element').get('x')
+        y = details.get('element').get('y')
+        width = details.get('element').get('width')
+        height = details.get('element').get('height')
+        logging.info('Drawing rectangle: %s %s %s %s' % (x, y, width, height))
+        draw.rectangle(((y, x), (height, width)), outline='yellow')
+        filename = instance.basename + str(time.time()) + details.get('name') + '.png'
+        image.save(filename)
+        logging.info("Annotated screenshot saved as '%s'" % filename)
--- python-webdriver/TestHarness/TestCase.py
+++ python-webdriver/TestHarness/TestCase.py
@@ -1,49 +1,66 @@
 import logging
-import os
 import sys
 import unittest
-
+import os
 import pytest
 
 from Flow import BasicFlow
+from I18n import I18nAuditor
+from TestConfig import TestRunConfig
 from TestHarness import WebDriverModule
 
 
 class TestCase(unittest.TestCase):
     """
-      Testcase base
+      Testcase base. The location for adding augments.
     """
     def setup_method(self, method):
         self.init_logging()
-        logging.info("\n%s:%s" % (type(self).__name__, method.__name__))
+        logging.info("Starting %s:%s" % (type(self).__name__, method.__name__))
+        # self.i18n_up()
 
     def teardown_method(self, method):
         logging.info("Finished test " + method.__name__)
+        # self.i18n_down()
         BasicFlow.BasicFlow().goto_logout()
-        WebDriverModule.WebDriverModule().reload_webdriver()
+        WebDriverModule.WebDriverModule().reload_driver()
 
     def init_logging(self):
-        loglevel = logging.INFO
-        loglevels = ["DEBUG", "INFO", "WARN"]
-        loglevelloc = os.environ.get("WEBDRIVERLOGLOC")
-        if "WEBDRIVERLOGLEVEL" in os.environ:
-            loglevelstr = os.environ["WEBDRIVERLOGLEVEL"]
-            if loglevelstr not in loglevels:
-                print("Invalid log level specified '%s', must be %s" % (loglevelstr, loglevels))
-            if loglevelstr == "DEBUG":
-                loglevel = logging.DEBUG
-            elif loglevelstr == "WARN":
-                loglevel = logging.WARN
-            else:
-                loglevel = logging.INFO
-        logging.basicConfig(filename=loglevelloc, filemode='w', level=loglevel)
+        log_levels = ["DEBUG", "INFO", "WARN"]
+        log_location = TestRunConfig.TestRunConfig().get("log_location")
+        log_level_str = str(TestRunConfig.TestRunConfig().get("log_level")).upper()
+        if log_level_str not in log_levels:
+            print("Invalid log level specified '%s', must be %s" % (log_level_str, log_levels))
+            raise Exception("Invalid log level specified")
+        elif log_level_str == "DEBUG":
+            log_level = logging.DEBUG
+        elif log_level_str == "WARN":
+            log_level = logging.WARN
+        else:
+            log_level = logging.INFO
+        logging.basicConfig(filename=log_location, filemode='w+', level=log_level)
         logging.StreamHandler(sys.stdout)
 
     @pytest.yield_fixture(scope="session", autouse=True)
     def driver_fixture(self):
+        # Shut down the driver when ALL tests are finished
         logging.info("Fixtures set")
         yield
         logging.info("Shutting down WebDriver")
         driverref = WebDriverModule.WebDriverModule().get_driver()
         driverref.close()
         driverref.quit()
+
+    # Augments
+    def i18n_up(self):
+        # Initiate I18nAuditor
+        locale = TestRunConfig.TestRunConfig().get('i18n_locale')
+        logging.debug("Locale is %s", locale)
+        if locale != 'en_US':
+            BasicFlow.BasicFlow().change_language(locale)
+        I18nAuditor.I18nAuditor().ready_state(True)
+
+    def i18n_down(self):
+        # Disable the I18nAuditor
+        logging.debug('Winding down I18nAuditor')
+        I18nAuditor.I18nAuditor().ready_state(False)
