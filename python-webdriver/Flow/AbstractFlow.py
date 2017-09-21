ee13ba5ccd07b21984352d7a144c03be7db06fc8
Sachin Pathare <spathare@redhat.com>
2017-09-21 23:33:53 +1000

    Flow fixes

--- python-webdriver/Flow/AbstractFlow.py
+++ python-webdriver/Flow/AbstractFlow.py
@@ -5,9 +5,10 @@ import os
 class AbstractFlow:
 
     HOMEPAGE = os.environ["WEBDRIVERHOME"]
+    driver = None
 
-    # def __init__(self):
-    #     self.driver = WebDriverModule.WebDriverModule().get_driver()
+    def __init__(self):
+        self.driver = WebDriverModule.WebDriverModule().get_driver()
 
     def get_host_url(self):
         return self.HOMEPAGE
--- python-webdriver/Flow/BasicFlow.py
+++ python-webdriver/Flow/BasicFlow.py
@@ -2,15 +2,14 @@ import logging
 
 from Flow.AbstractFlow import AbstractFlow
 from Page.HomePage import HomePage
-from TestHarness import WebDriverModule
+from selenium.webdriver.common.by import By
 
 
 class BasicFlow(AbstractFlow):
-
-    driver = None
-
-    def __init__(self):
-        self.driver = WebDriverModule.WebDriverModule().get_driver()
+    """
+    Contains the basic workflow actions, such as go to home
+    """
+    LANGUAGE_SELECT = (By.ID, 'localeBox')
 
     def goto_home(self):
         logging.info("Go to homepage %s" % self.HOMEPAGE)
@@ -19,5 +18,10 @@ class BasicFlow(AbstractFlow):
 
     def goto_logout(self):
         logging.info("Go to logout directly")
-        logouturl = self.get_host_url()+"/logout"
-        self.driver.get(logouturl)
+        logout_url = self.get_host_url() + "/logout"
+        self.driver.get(logout_url)
+
+    def change_language(self, language):
+        logging.info('Changing language to %s', language)
+        homepage = self.goto_home()
+        homepage.select_option(self.LANGUAGE_SELECT, language)
