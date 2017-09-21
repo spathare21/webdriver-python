313676feb19a43a6cb03c34813023b8c1e77b0ca
Sachin Pathare <spathare@redhat.com>
2017-09-21 23:26:41 +1000

    Further modularise WebDriver

--- python-webdriver/TestHarness/WebDriverModule.py
+++ python-webdriver/TestHarness/WebDriverModule.py
@@ -1,37 +1,86 @@
 import logging
-import os
 
 from selenium import webdriver
 from selenium.webdriver.support.events import EventFiringWebDriver
 
+from TestConfig import TestRunConfig
 from TestHarness import ScreenshotListener
 
 
 class WebDriverModule:
+    """
+      Provides a persistent driver for all tests and modules to access
+    """
     driver = None
+    screenshot_listener = None
 
     @staticmethod
     def get_driver():
+        """
+        Get the driver, creating if it does not exist
+        :return: WebDriver object
+        """
         if not WebDriverModule.driver:
-            WebDriverModule.driver = WebDriverModule.create_webdriver()
-            logging.warning("Driver created")
+            WebDriverModule.driver = WebDriverModule.create_driver()
+            logging.info("Driver created")
+            WebDriverModule.register_screenshot_listener()
         return WebDriverModule.driver
 
     @staticmethod
-    def create_webdriver():
+    def create_driver():
+        """
+        Create a WebDriver object
+        :return: new WebDriver object
+        """
         logging.info("Initialise WebDriver")
-        driverpath = "chromedriver"
-        if "CHROMEDRIVER" in os.environ:
-            driverpath = os.environ.get("CHROMEDRIVER")
-        chromeOptions = webdriver.ChromeOptions()
-        basedriver = webdriver.Chrome(executable_path=driverpath,port=4444,chrome_options=chromeOptions)
-        return EventFiringWebDriver(basedriver, ScreenshotListener.ScreenshotListener())
+        return WebDriverModule.chrome_driver()
 
     @staticmethod
-    def reload_webdriver():
+    def chrome_driver():
+        """
+        Create a basic chromedriver object
+        :return: Chrome driver object
+        """
+        driver_path = TestRunConfig.TestRunConfig().get("driver_path")
+        chrome_options = webdriver.ChromeOptions()
+        return webdriver.Chrome(executable_path=driver_path,port=4444,chrome_options=chrome_options)
+
+    @staticmethod
+    def register_screenshot_listener():
+        """
+        Upgrade the driver to an event firing driver, with screenshot listener attached
+        :return: EventFiringWebDriver
+        """
+        WebDriverModule.screenshot_listener = ScreenshotListener.ScreenshotListener()
+        WebDriverModule.driver = EventFiringWebDriver(WebDriverModule.driver, WebDriverModule.screenshot_listener)
+
+    @staticmethod
+    def reload_driver():
+        """
+        Clear cookies and reload, for a 'fresh' web page
+        :return:
+        """
         logging.info("Reloading WebDriver")
-        logging.info("Cookies: %s" % WebDriverModule.driver.get_cookies())
+        logging.debug("Cookies: %s" % WebDriverModule.driver.get_cookies())
         WebDriverModule.driver.delete_all_cookies()
         WebDriverModule.driver.refresh()
-        logging.info("Cookies/2: %s" % WebDriverModule.driver.get_cookies())
+        logging.debug("Cookies/2: %s" % WebDriverModule.driver.get_cookies())
 
+    @staticmethod
+    def inject_screenshot(name):
+        """
+        Notify the screenshot listener to take a screenshot with given name
+        :param name: identifier for this screenshot
+        :return: None
+        """
+        WebDriverModule.screenshot_listener.custom_screenshot(WebDriverModule.driver, name)
+
+    @staticmethod
+    def inject_annotated_screenshot(details):
+        """
+        Experimental. Take a screenshot with sections to highlight
+        :param details: Dict of
+          {'name': name, element: {'location': {'x': x, 'y': y}, 'size': {'width': w, 'height': h}}}
+        :return: None
+        """
+        WebDriverModule.screenshot_listener.annotated_screenshot(WebDriverModule.driver, details)
